13a3f5dcda91d8aff6a65ba498167b3085ef449e
Jeritt Williams <49690937+jeritt@users.noreply.github.com>
2023-06-06 13:30:11 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,13 @@
 function lastWindDir () {
     if (weatherbit.windDirection() != "???") {
-        oldWindDir = weatherbit.windDirection()
         curentWindDir = weatherbit.windDirection()
-    } else {
-        curentWindDir = oldWindDir
     }
 }
 input.onButtonPressed(Button.B, function () {
+    lastWindDir()
     serial.writeLine("Wind Speed (mph): " + Math.trunc(weatherbit.windSpeed()) + " , " + "Wind Direction: " + curentWindDir)
 })
 let curentWindDir = ""
-let oldWindDir = ""
 serial.redirectToUSB()
 weatherbit.startWindMonitoring()
 basic.forever(function () {
